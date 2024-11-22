7d80244ec775129d252374903097761087e7405d
borapapila <papilabora@gmail.com>
2024-11-22 20:23:54 +0300

    version 1

--- src/main/java/com/borapapila/the_papila_bank/service/UserService.java
+++ src/main/java/com/borapapila/the_papila_bank/service/UserService.java
@@ -1,6 +1,5 @@
 package com.borapapila.the_papila_bank.service;
 
-import com.borapapila.the_papila_bank.dto.AccountInfo;
 import com.borapapila.the_papila_bank.dto.BankResponse;
 import com.borapapila.the_papila_bank.dto.UserRequest;
 
